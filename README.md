084a39897b12dbb8f162619026d60b0b319c26b1
Jesus Alvarez <jeezusjr@gmail.com>
2022-05-25 21:22:51 -0700

    begin adding highlighting

--- README.md
+++ README.md
@@ -2,10 +2,31 @@
 
 [Jakt](https://github.com/SerenityOS/jakt) grammar for [tree-sitter](https://github.com/tree-sitter/tree-sitter)
 
+Jakt is a memory safe programming language built for SerenityOS that transpiles to C++.
+
 Work in progress.
 
 ## How to run tests
 
 ```
 tree-sitter generate && tree-sitter test -d -f "Primitive types"
+```
+
+## How to install in Neovim
+
+Add the following to the neovim config:
+
+```
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.jakt = {
+  install_info = {
+    url = "~/projects/tree-sitter-zimbu", -- local path or git repo
+    files = {"src/parser.c"},
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "zu", -- if filetype does not match the parser name
+}
 ```
---
+++ queries/highlights.scm
@@ -0,0 +1,76 @@
+; Identifier conventions
+
+; Assume all-caps names are constants
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z\\d_]+$'"))
+
+; Assume other uppercase names are enum constructors
+((identifier) @constructor
+ (#match? @constructor "^[A-Z]"))
+
+; Function calls
+
+(call_expression
+  function: (identifier) @function)
+
+; Function definitions
+
+(function_declaration (identifier) @function)
+
+; Other identifiers
+
+(type_identifier) @type
+(primitive_type) @type.builtin
+
+(line_comment) @comment
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+; "::" @punctuation.delimiter
+":" @punctuation.delimiter
+; "." @punctuation.delimiter
+"," @punctuation.delimiter
+
+(parameter (identifier) @variable.parameter)
+
+; "break" @keyword
+; "const" @keyword
+"continue" @keyword
+; "default" @keyword
+"else" @keyword
+; "enum" @keyword
+"function" @keyword
+"for" @keyword
+"if" @keyword
+"in" @keyword
+"let" @keyword
+; "loop" @keyword
+; "match" @keyword
+"return" @keyword
+; "static" @keyword
+; "struct" @keyword
+; "type" @keyword
+; "union" @keyword
+; "unsafe" @keyword
+; "where" @keyword
+"while" @keyword
+(mutable_specifier) @keyword
+
+; (self) @variable.builtin
+
+(char_literal) @string
+(string_literal) @string
+
+(boolean_literal) @constant.builtin
+(integer_literal) @constant.builtin
+(float_literal) @constant.builtin
+
+; (escape_sequence) @escape
+
+"*" @operator
+"&" @operator
