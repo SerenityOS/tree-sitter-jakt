443ee217e56f6d70027717b7efbaa7f3dfa8e6a8
Jesus Alvarez <jeezusjr@gmail.com>
2022-05-31 00:49:35 -0700

    WIP roadmapgen.py first test passes

---
+++ script/README.md
@@ -0,0 +1,33 @@
+# Helper scripts
+
+## A note about python scripts
+
+The python scripts have dependencies on third party python modules. These can be easily
+retrieved using [poetry](https://python-poetry.org/). See "how to run tests" section
+below.
+
+## roadmapgen.py
+
+A script to compare Jakt Samples with implemented tree-sitter-jakt tests to ensure nothing
+is missed.
+
+This is necessary because jakt is new and does not have a spec yet. There are already (as
+of Jun 2022 a few hundred tests and that number will only increase.
+
+### How to run tests for roadmapgen
+
+Prereqs
+
+1. The jakt source code containing the samples
+1. Create virtualenv and install poetry
+1. Run "poetry install"
+1. Run pytest
+
+```
+$ cd tree-sitter-jakt/
+$ python -m venv myvenv
+$ ./myvenv/bin/activate
+$ pip install poetry
+$ poetry install
+$ JAKT_SRC_PATH=/opt/src/jakt python -m pytest script/test
+```
--- script/roadmapgen.py
+++ script/roadmapgen.py
@@ -14,12 +14,32 @@ This script also has a mechanism to ensure if a test is changed, it will be mark
 
 import os
 import pathlib
+import re
 
 import typer
 
 from rich.console import Console
 
 console = Console()
+
+
+def build_test_list(jakt_path: str):
+    """ """
+    list_of_files: list = []
+    for root, dirs, files in os.walk(pathlib.Path(jakt_path + "samples"), topdown=True):
+        for name in files:
+            error_file = pathlib
+            if any(x in name for x in [".error", ".err", ".out"]) or re.findall(
+                r"_bad[\d]?", name
+            ):
+                continue
+            if (
+                pathlib.Path(root, f"{name[:-5]}.error").exists()
+                or pathlib.Path(root, f"{name[:-5]}.err").exists()
+            ):
+                continue
+            list_of_files.append(pathlib.Path(root, name))
+    return list_of_files
 
 
 def main(
@@ -29,24 +49,7 @@ def main(
 ):
     """A tool for keeping track of Jakt parsing implementation."""
     console.log("Started")
-
-    for root, dirs, files in os.walk(
-        pathlib.Path(jakt_path + "samples"), topdown=False
-    ):
-        for name in files:
-            error_file = pathlib
-            if any(x in name for x in [".error", ".err", ".out"]):
-                console.log(f"skipping {pathlib.Path(root, name)}")
-                continue
-            if (
-                pathlib.Path(root, f"{name[:-5]}.error").exists()
-                and pathlib.Path(root, f"{name[:-5]}.err").exists()
-            ):
-                console.log(f"skipping {pathlib.Path(root, name)}")
-                continue
-            console.log(os.path.join(root, name))
-        # for name in dirs:
-        #     console.log(os.path.join(root, name))
+    build_test_list(jakt_path)
 
 
 if __name__ == "__main__":
