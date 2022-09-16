f880ae8ecddfedc55381ed38547f0de143614ebc
Jesus Alvarez <jeezusjr@gmail.com>
2022-09-15 22:05:14 -0700

    nvim-lua: visual select + f2 to run treesitter tests
    .nvim-commands.lua is a lua file containing commands for neovim that add
    keybindings for this project to the running instance. It has only a
    single dependency on toggleterm (not including tree-sitter cli) which is
    used to improve the terminal workflow in neovim.
    
    Source with `:luafile %%`
    
    After that press the F1 key to run the entire test suite.
    
    To run a single test, visual select the test name in the corpus text
    file and press F2.
    
    Press F3 to run the py.test test suite for roadmapgen.

--- .nvim-commands.lua
+++ .nvim-commands.lua
@@ -1,19 +1,27 @@
--- JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test
--- tree-sitter generate && tree-sitter test
--- command! -count=1 TermGitPush  lua require'toggleterm'.exec("git push",    <count>, 12)
---
--- python script/roadmapgen.py --jakt-path ~/src/jakt/jakt/
+-- source with luafile to run tree sitter test commands in the integrated terminal
+-- DEPENDS ON https://github.com/akinsho/toggleterm.nvim
 
 vim.api.nvim_set_keymap("n", "<F1>", "<cmd>TermExec cmd='tree-sitter generate && tree-sitter test'<CR>", {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<F2>", "<cmd>TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"Closures generic closure with parameter\"'<CR>", {noremap = true, silent = true})
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
 
-vim.api.nvim_set_keymap("n", "<F3>", "<cmd>TermExec cmd='python script/roadmapgen.py update readme --jakt-path ~/src/jakt/jakt/'<CR>", {noremap = true, silent = true})
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
 
-vim.api.nvim_set_keymap("n", "<F4>", "<cmd>TermExec cmd='python script/roadmapgen.py update state --jakt-path ~/src/jakt/jakt/ --single corpus/ranges/range.txt'<CR>", {noremap = true, silent = true})
+vim.keymap.set("v", "<F2>", function()
+    local test = vim.getVisualSelection()
+    vim.cmd("TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"" .. test .. "\"'<CR>")
+end, {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<F5>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test'<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<F3>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test'<CR>", {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<F6>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test -k \"test_calculate_tests_completed\"'<CR>", {noremap = true, silent = true})
-
-vim.api.nvim_set_keymap("n", "<F7>", "<cmd>TermExec cmd='python script/roadmapgen.py check --jakt-path ~/src/jakt/jakt/'<CR>", {noremap = true, silent = true})
+-- TODO update this to the visual select thingy
+-- vim.api.nvim_set_keymap("n", "<F4>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test -k \"test_calculate_tests_completed\"'<CR>", {noremap = true, silent = true})
