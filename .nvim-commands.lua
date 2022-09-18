08f60999e2f3225cc128a31c2491a5a561dddbcb
Jesus Alvarez <jeezusjr@gmail.com>
2022-09-17 20:53:32 -0700

    nvim-commands: add support for triggering arbitrary commands
    In toggleterm

--- .nvim-commands.lua
+++ .nvim-commands.lua
@@ -16,12 +16,26 @@ function vim.getVisualSelection()
 	end
 end
 
+TSTEST = ""
+
+-- run a test with the visually selected string
 vim.keymap.set("v", "<F2>", function()
-    local test = vim.getVisualSelection()
-    vim.cmd("TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"" .. test .. "\"'<CR>")
+    TSTEST = vim.getVisualSelection()
+    vim.cmd("TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"" .. TSTEST .. "\" -D'<CR>")
 end, {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<F3>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test'<CR>", {noremap = true, silent = true})
+-- rerun last visually selected test
+vim.keymap.set("n", "<F2>", function()
+    vim.cmd("TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"" .. TSTEST .. "\" -D'<CR>")
+end, {noremap = true, silent = true})
+
+-- update a test
+vim.keymap.set("v", "<F3>", function()
+    TSTEST = vim.getVisualSelection()
+    vim.cmd("TermExec cmd='tree-sitter generate && tree-sitter test -d -f \"" .. TSTEST .. "\" -u'<CR>")
+end, {noremap = true, silent = true})
+
+-- vim.api.nvim_set_keymap("n", "<F3>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test'<CR>", {noremap = true, silent = true})
 
 -- TODO update this to the visual select thingy
 -- vim.api.nvim_set_keymap("n", "<F4>", "<cmd>TermExec cmd='JAKT_SRC_PATH=~/src/jakt/jakt/ python -m pytest script/test -k \"test_calculate_tests_completed\"'<CR>", {noremap = true, silent = true})
