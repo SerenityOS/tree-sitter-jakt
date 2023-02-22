024e1f14ef81dad8d5b92b11a5489e084db2fabb
Jesus Alvarez <jeezusjr@gmail.com>
2023-02-22 13:39:36 -0800

    roadmapgen: add sync command
    Syncs the tree-sitter corpus test suite with the upstream Jakt samples
    test suite. Any files in the state file that exist in the tree-sitter
    test suite that no longer exist upstream are deleted.
    
    The state file is overwritten with this command.

--- script/roadmapgen.py
+++ script/roadmapgen.py
@@ -19,6 +19,11 @@ TODO:
       This will only be flagged when the roadmap.py script is ran
 """
 
+from rich.console import Console
+from rich.table import Table
+from rich.style import Style
+from rich import box
+
 import os
 import pathlib
 import re
@@ -27,6 +32,8 @@ from datetime import date
 import fileinput
 import hashlib
 import sys
+import random
+import string
 from typing import Any, Optional
 from types import SimpleNamespace
 from enum import Enum
@@ -41,11 +48,6 @@ update_app = typer.Typer()
 state_app = typer.Typer()
 app.add_typer(update_app, name="update")
 update_app.add_typer(state_app, name="state")
-
-from rich.console import Console
-from rich.table import Table
-from rich.style import Style
-from rich import box
 
 console = Console()
 
@@ -74,7 +76,7 @@ def build_test_list(jakt_path: str) -> list:
     for root, _, files in os.walk(pathlib.Path(jakt_path, "samples"), topdown=True):
         for name in files:
             test_path = pathlib.Path(root, name)
-            if not ".jakt" in str(test_path):
+            if ".jakt" not in str(test_path):
                 console.log(f"Skipping file: {test_path}")
                 continue
             test = test_path.read_text()
@@ -97,7 +99,7 @@ def build_corpus_list() -> dict[str, list]:
             # Strip out scheme comments
             stripped_text = file_text.replace("; ", "").replace(";", "")
             for x in re.findall(corpus_test_header_pattern, stripped_text):
-                if not file_path_posix in list_of_tests:
+                if file_path_posix not in list_of_tests:
                     list_of_tests[file_path_posix] = []
                 list_of_tests[file_path_posix].append(x)
 
@@ -148,7 +150,10 @@ def _update_readme(tree_sitter_tests: dict[str, list], jakt_tests: list):
     """Updates the main tree-sitter-jakt readme with percent completed."""
     count, num_tests, perc = calculate_tests_completed(tree_sitter_tests, jakt_tests)
     date_now = date.today().strftime("%B %-d, %Y")
-    perc_line = f"tree-sitter-jakt implements {count:.2f} of {num_tests} ({perc:.1f}%) of passable Jakt samples as of {date_now}"
+    perc_line = (
+        f"tree-sitter-jakt implements {count:.2f} of {num_tests} ({perc:.1f}%) of passable Jakt samples as of "
+        + date_now
+    )
     console.log(f"New percentage line: '{perc_line}'")
     with fileinput.input(pathlib.Path("README.md"), inplace=True) as f:
         for line in f:
@@ -301,8 +306,8 @@ def build_test_map(jakt_path: str) -> TestMap:
     for root, _, files in os.walk(pathlib.Path(jakt_path, "samples"), topdown=True):
         for name in files:
             file_path = pathlib.Path(root, name)
-            if not ".jakt" in str(file_path):
-                console.log(f"Skipping file: {file_path}")
+            if ".jakt" not in str(file_path):
+                console.log(f"skipping file: {file_path}")
                 continue
             file_text = file_path.read_text()
             hash = hashlib.md5(file_text.encode("utf-8")).hexdigest()
@@ -329,7 +334,7 @@ def test_from_corpus_path(corpus_path: str, jakt_path: str) -> Optional[Test]:
     """Create a list of jakt samples"""
     if ".jakt" in corpus_path:
         console.log(f"Skipping file: {corpus_path}")
-        return
+        return None
     file_path = pathlib.Path(corpus_path)
     stripped_corpus_path = file_path.parts[2:-1]
     sample_path = pathlib.Path(
@@ -651,6 +656,164 @@ def update(
     ctx.obj = SimpleNamespace(jakt_path=jakt_path)
 
 
+@app.command()
+def sync(
+    ctx: typer.Context,
+    jakt_path: str = typer.Option(
+        ..., help="The path to the Jakt source code containing the samples directory"
+    ),
+    dry_run: bool = typer.Option(
+        default=False, help="Show changes table, but don't do anything"
+    ),
+):
+    """Update the treesitter corpus tests with jakt sample sources"""
+    if dry_run:
+        update_corpus_print_task_table(ctx.obj.jakt_path)
+        return
+    console.log("syncing corpus test suite with Jakt sample suite")
+    map = build_test_map(jakt_path)
+    sf = load_state_file(jakt_path)
+    state_sync_deleted(sf)
+    for _, v in enumerate(map.map):
+        fill_test_props(sf, v)
+        if v.corpus_file_path.exists() and v.deleted:
+            v.corpus_file_path.unlink()
+            console.log(f"deleted '{v.corpus_file_path}'")
+            continue
+        sample_content: bytes
+        with open(v.jakt_sample_path, "rb") as f:
+            sample_content = f.read()
+        v.corpus_file_path.parent.mkdir(0o744, True, True)
+        if not v.corpus_file_path.exists():
+            write_new_corpus_test(v.corpus_file_path, sample_content)
+            continue
+        update_corpus_test(v.corpus_file_path, sample_content)
+    write_state_file(map)
+    console.log(
+        "[yellow][bold]WARNING[/yellow] - "
+        + "please run the `check` command to run tree-sitter tests to finish updating the state[/bold]"
+    )
+
+
+def state_sync_deleted(statefile: TestMap):
+    """Compares the statefile and corpus suite for deleted Jakt samples upstream."""
+    for sftest in statefile.map:
+        if sftest.jakt_sample_path.exists():
+            continue
+        rel_path = sftest.corpus_file_path.relative_to(os.getcwd())
+        console.log(
+            f"found '{rel_path}' in statefile, but test has been deleted upstream"
+        )
+        if not sftest.corpus_file_path.exists():
+            console.log(f"'{rel_path}' does not exist, removing from state file")
+            continue
+        sftest.corpus_file_path.unlink()
+        console.log(f"'{rel_path}' has been deleted")
+
+
+def fill_test_props(statefile: TestMap, mytest: Test):
+    """Checks a single Test against the state file"""
+    sf_equiv = statefile.get_by_jakt_sample_path(str(mytest.jakt_sample_path))
+    if not sf_equiv:
+        mytest.new = True
+    elif mytest.jakt_sample_hash != sf_equiv.jakt_sample_hash:
+        mytest.changed = True
+
+
+def update_corpus_print_task_table(jakt_path: str):
+    """Print a list of tasks to be done to update the TS corpus tests"""
+    map = build_test_map(jakt_path)
+    sf = load_state_file(jakt_path)
+
+    table = Table(box=box.MINIMAL_DOUBLE_HEAD)
+    table.add_column("Counter", justify="left")
+    table.add_column("Sample", justify="left")
+    table.add_column("Task", justify="center")
+
+    for k, v in enumerate(map.map):
+        fill_test_props(sf, v)
+        if v.falty:
+            color = Style(color=None, dim=True)
+            table.add_row(
+                str(k + 1), str(v.jakt_sample_path), "ignore: falty", style=color
+            )
+            continue
+        if not v.corpus_file_path.exists():
+            color = Style(color="green", bold=True)
+            table.add_row(str(k + 1), str(v.corpus_file_path), "create", style=color)
+            continue
+        if v.changed:
+            color = Style(color="yellow", bold=True)
+            table.add_row(str(k + 1), str(v.corpus_file_path), "update", style=color)
+
+    for k, v in enumerate(sf.map):
+        if not map.get_by_jakt_sample_path(str(v.jakt_sample_path)):
+            color = Style(color="red", bold=True)
+            table.add_row(str(-1), str(v.corpus_file_path), "deleted", style=color)
+
+    console.log(table)
+
+
+def corpus_test_banner(title: str) -> str:
+    """Wrap a test title in banner ascii."""
+    eqlb = "=" * 80
+    return f"{eqlb}\n{title}\n{eqlb}\n\n"
+
+
+def corpus_test_title(corpus_file_path) -> str:
+    """Returns a test title generated from test path and name.
+
+    The test title needs to be unique for each test, so as a precaution, a random string
+    is added to aid in this requirement.
+    """
+    random_id = "".join(random.choices(string.ascii_uppercase + string.digits, k=7))
+    ts_test_name = (
+        " ".join(corpus_file_path.relative_to(os.getcwd()).parts[2:])
+        .replace(".txt", "")
+        .replace("_", " ")
+    ).title()
+    uid = f"id={random_id}".upper()
+    return f"{ts_test_name} {uid}"
+
+
+def write_new_corpus_test(corpus_file_path: pathlib.Path, sample_content: bytes):
+    """Writes a new corpus test using sample_content and a default s-expression.
+
+    NOT IMPLEMENTED is appended to the test title so the check command will show that it
+    has not been implemented yet. The Tree Sitter CLI does not have good support for
+    adding tests but not running them until they are actually implemented.
+
+    In addition, the ';' character is prepended to each line of the test as a hack to
+    prevent TS CLI from running the test so the failing test does not pollute the TS test
+    suite with false negatives.
+    """
+    ts_title = f"NOT IMPLEMENTED {corpus_test_title(corpus_file_path)}"
+    ts_banner = corpus_test_banner(ts_title)
+    console.log(f"creating new test: {ts_title!r}")
+    sepb = "-" * 80
+    content = f"{ts_banner}{sample_content.decode()}\n{sepb}\n\n(source_file)"
+    commented_content = ""
+    for lin in content.splitlines():
+        commented_content += "; " + lin + "\n"
+    with open(corpus_file_path, "w") as f:
+        f.write(commented_content)
+
+
+def update_corpus_test(corpus_file_path: pathlib.Path, sample_content: bytes):
+    """Update an existing corpus test with content from the jakt sample"""
+    with open(corpus_file_path, "rb") as f:
+        corpus_content = f.read()
+    if m := re.match(r"(?s)=+\s+[\w ]+\s+=+\s+.+\s+-+\s+(.+)", corpus_content.decode()):
+        ts_title = corpus_test_title(corpus_file_path)
+        ts_banner = corpus_test_banner(ts_title)
+        console.log(f"updating existing test: {ts_title!r}")
+        sepb = "-" * 80
+        sexpr = m.groups()[0]
+        content_new = f"{ts_banner}{sample_content.decode()}\n{sepb}\n\n{sexpr}"
+        with open(corpus_file_path, "w") as f:
+            f.write(content_new)
+
+
 @update_app.command("readme")
 def update_readme(ctx: typer.Context):
     """Update the README with percentage completed"""
@@ -666,39 +829,37 @@ def update_test(
 ):
     """Update the Jakt sample state for a single test."""
     if not state_file.exists():
-        console.log(f"[red][ERROR] - State file {state_file} does not exist![/red]")
+        console.log(f"[red][ERROR] - state file {state_file} does not exist[/red]")
         sys.exit(1)
 
     sf = load_state_file(ctx.obj.jakt_path)
 
     if not (test := sf.get_by_corpus_path(path)):
-        console.log(f"Adding single test to state file for '{path}'")
+        console.log(f"adding single test to state file for '{path}'")
         if not (new_test := test_from_corpus_path(path, ctx.obj.jakt_path)):
-            console.log(
-                f"[red][ERROR] - Could not update '{path}' in state file![/red]"
-            )
+            console.log(f"[red][ERROR] - could not update '{path}' in state file[/red]")
             sys.exit(1)
         sf.map.append(new_test)
     else:
         if not pathlib.Path(test.jakt_sample_path).exists():
-            console.log("Removing deleted sample from state file")
+            console.log("removing deleted sample from state file")
             if not sf.remove_by_corpus_path(path):
                 console.log(
-                    f"[red][ERROR] - Could not remove test by corpus path '{path}' from state file![/red]"
+                    f"[red][ERROR] - could not remove test by corpus path '{path}' from state file[/red]"
                 )
                 sys.exit(1)
             else:
                 write_state_file(sf)
-                console.log(f"Test '{path}' removed from state file")
+                console.log(f"test '{path}' removed from state file")
                 return
-        console.log(f"Updating state of single test: '{path}'")
+        console.log(f"updating state of single test: '{path}'")
         console.log(f"jakt sample path: '{test.jakt_sample_path}'")
-        console.log(f"Old hash: {test.jakt_sample_hash}")
+        console.log(f"old hash: {test.jakt_sample_hash}")
         hash = hashlib.md5(
             test.jakt_sample_path.read_text().encode("utf-8")
         ).hexdigest()
         test.jakt_sample_hash = hash
-        console.log(f"New hash: {test.jakt_sample_hash}")
+        console.log(f"new hash: {test.jakt_sample_hash}")
 
     write_state_file(sf)
 
@@ -710,7 +871,4 @@ def update_state_all(ctx: typer.Context):
 
 
 if __name__ == "__main__":
-    console.log(
-        "[bold][yellow]WARNING[/yellow]: this script assumes tests are in passing state[/bold]"
-    )
     app()
